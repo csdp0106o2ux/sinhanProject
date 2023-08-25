93487dd10b3dc6626f7871befa7fcf0aba767819
csdp0106o2ux <csdp0106@o2ux.com>
2023-08-25 17:33:15 +0900

    feat: add contents inside GoodCard components

--- src/layout/TopSlideSheet/components/GoodCard.jsx
+++ src/layout/TopSlideSheet/components/GoodCard.jsx
@@ -7,8 +7,6 @@ const Container = styled.div`
     padding: 20px;
     width: 560px;
     height: 332px;
-    display: flex;
-    flex-direction: column;
     border-radius: 16px;
     background-color: ${(props) => props.theme.aliceBlue};
 `;
@@ -70,6 +68,50 @@ const RateText = styled.p`
     color: ${(props) => (props.blue ? props.theme.lightPacificBlue : props.theme.freeSpeechBlue)};
 `;
 
+const ScoreWrap = styled.div`
+    padding: 16px;
+    width: 206px;
+    height: 252px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    border-radius: 16px;
+    background-color: #fff;
+`;
+
+const ScoreTitleWrap = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const UnitWrap = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const ScoreTitle = styled.p`
+    font-family: NanumSquareOTFB;
+    font-size: 14px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.5;
+    letter-spacing: normal;
+    color: #333;
+`;
+const ScoreText = styled.p`
+    font-family: ${(props) => (props.unit ? 'NanumSquareOTFR' : 'NanumSquareOTFB')};
+    font-size: 18px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.56;
+    letter-spacing: normal;
+    text-align: right;
+    color: #1f1f1f;
+`;
+
 const GoodCard = () => {
     return (
         <Container>
@@ -86,6 +128,53 @@ const GoodCard = () => {
                         <RateText>90.5%</RateText>
                     </GraphTitleWrap>
                 </GraphWrap>
+                <ScoreWrap>
+                    <ScoreTitleWrap>
+                        <ScoreTitle>Good Score</ScoreTitle>
+                        <UnitWrap>
+                            <UnitWrap>
+                                <ScoreText>84.1</ScoreText>
+                                <ScoreText>점</ScoreText>
+                            </UnitWrap>
+                        </UnitWrap>
+                    </ScoreTitleWrap>
+                    <ScoreTitleWrap>
+                        <ScoreTitle>Good 경험지수</ScoreTitle>
+                        <UnitWrap>
+                            <UnitWrap>
+                                <ScoreText>84.1</ScoreText>
+                                <ScoreText>점</ScoreText>
+                            </UnitWrap>
+                        </UnitWrap>
+                    </ScoreTitleWrap>
+                    <ScoreTitleWrap>
+                        <ScoreTitle>충성도</ScoreTitle>
+                        <UnitWrap>
+                            <UnitWrap>
+                                <ScoreText>84.1</ScoreText>
+                                <ScoreText>점</ScoreText>
+                            </UnitWrap>
+                        </UnitWrap>
+                    </ScoreTitleWrap>
+                    <ScoreTitleWrap>
+                        <ScoreTitle>Good 응답수</ScoreTitle>
+                        <UnitWrap>
+                            <UnitWrap>
+                                <ScoreText>15</ScoreText>
+                                <ScoreText>건</ScoreText>
+                            </UnitWrap>
+                        </UnitWrap>
+                    </ScoreTitleWrap>
+                    <ScoreTitleWrap>
+                        <ScoreTitle>칭찬VOC</ScoreTitle>
+                        <UnitWrap>
+                            <UnitWrap>
+                                <ScoreText>5</ScoreText>
+                                <ScoreText>건</ScoreText>
+                            </UnitWrap>
+                        </UnitWrap>
+                    </ScoreTitleWrap>
+                </ScoreWrap>
             </Wrapper>
         </Container>
     );
